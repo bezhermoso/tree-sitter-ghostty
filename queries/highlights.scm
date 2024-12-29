8e9277d31158ff87bba2cc7107237fce4bb44c2c
Bez Hermoso <bezalelhermoso@gmail.com>
2024-12-29 10:35:00 -0800

    chore: add comments to highlights.scm

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -9,28 +9,33 @@
 (boolean) @boolean
 
 [
- (number)
- (adjustment)
-] @number
-
-[
  (string)
  (color)
 ] @string
 
+[
+ (number)
+ (adjustment)
+] @number
+
+; (color) are hex values
+(color "#" @punctuation.delimiter.special
+  (#eq? @punctuation.delimiter.special "#"))
+
+; `palette`
 (palette_index) @variable.member
 
 (palette_value "=" @operator
   (#eq? @operator "="))
 
+; `config-file`
+(config_file_directive (property) @keyword.import)
 (config_file_directive (path_value) @string.special.path)
 
-(config_file_directive (property) @keyword.import)
-
-; Keybinds
+; `keybind`
 (keybind_value) @string.special
 
-
+; clear is a special keyword that clear all existing keybind up to that point
 ((keybind_value) @keyword
                  (#eq? @keyword "clear"))
 
@@ -40,16 +45,16 @@
 [
  "+"
  "="
+ (keybind_trigger ">")
 ] @operator
 
-(keybind_trigger ">") @operator
-
-(color "#" @punctuation.delimiter.special
-  (#eq? @punctuation.delimiter.special "#"))
-
+; NOTE: The order here matters!
 [
  (modifier_key)
  (key)
 ] @constant.builtin
 
-[(key_qualifier) (keybind_modifier)] @attribute
+[
+ (key_qualifier)
+ (keybind_modifier)
+] @attribute
