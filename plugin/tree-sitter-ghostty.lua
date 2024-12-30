d7af8771c549bdc53ce6e415d1a6c41cb1c3b8df
Bez Hermoso <bezalelhermoso@gmail.com>
2024-12-30 07:35:47 -0800

    feat: support nvim-treesitter rewrite

--- plugin/tree-sitter-ghostty.lua
+++ plugin/tree-sitter-ghostty.lua
@@ -1,16 +1,25 @@
 
-local _, ok = pcall(require, "nvim-treesitter.parsers")
+local ok, parsers = pcall(require, "nvim-treesitter.parsers")
 if not ok then
   -- nvim-treesitter not found.
   return
 end
+
 
 -- Register the plugin path as a tree-sitter parser to nvim-treesitter.
 -- This is not required, but it allows nvim-treesitter to manage it e.g. :TS{Install,Update} ghostty
 local file = debug.getinfo(1).source:match("@(.*/)")
 local plugin_dir = vim.fn.fnamemodify(file, ":p:h:h")
 
-local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+--- @type ParserInfo[]
+local parser_configs
+
+if type(parsers.get_parser_configs) == "function" then
+  parser_configs = parsers.get_parser_configs()
+else
+  -- "nvim-treesitter.parsers" exports the parser table itself starting nvim-treesitter v1.x
+  parser_configs = parsers
+end
 
 parser_configs.ghostty = {
   install_info = {
