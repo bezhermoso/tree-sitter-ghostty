e0c6d7d8a45a391955ebda74afebadef9b9ac60d
Bez Hermoso <bezalelhermoso@gmail.com>
2024-12-30 20:43:22 -0800

    refactor: move nvim-treesitter registration code

---
+++ lua/tree-sitter-ghostty/init.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+M.register_parser = function()
+    local ok, parsers = pcall(require, "nvim-treesitter.parsers")
+    if not ok then
+        return
+    end
+
+    -- Register the plugin path as a tree-sitter parser to nvim-treesitter.
+    -- This is not required, but it allows nvim-treesitter to manage it e.g. :TS{Install,Update} ghostty
+    local file = debug.getinfo(1).source:match("@(.*/)")
+    local plugin_dir = vim.fn.fnamemodify(file, ":p:h:h")
+
+    --- @type ParserInfo[]
+    local parser_configs
+
+    if type(parsers.get_parser_configs) == "function" then
+        parser_configs = parsers.get_parser_configs()
+    else
+        -- "nvim-treesitter.parsers" exports the parser table itself starting nvim-treesitter v1.x
+        parser_configs = parsers
+    end
+
+    if type(parser_configs.ghostty) == "table" then
+        -- A ghostty parser is already registered. Avoid overwriting it.
+        return
+    end
+
+    parser_configs.ghostty = {
+        install_info = {
+            url = plugin_dir,
+            files = { "src/parser.c" },
+            branch = "main",
+        },
+        requires_generate_from_grammar = true,
+    }
+end
+
+return M
--- plugin/tree-sitter-ghostty.lua
+++ plugin/tree-sitter-ghostty.lua
@@ -1,35 +1,3 @@
-
-local ok, parsers = pcall(require, "nvim-treesitter.parsers")
-if not ok then
-  -- nvim-treesitter not found.
-  return
-end
-
-
--- Register the plugin path as a tree-sitter parser to nvim-treesitter.
--- This is not required, but it allows nvim-treesitter to manage it e.g. :TS{Install,Update} ghostty
-local file = debug.getinfo(1).source:match("@(.*/)")
-local plugin_dir = vim.fn.fnamemodify(file, ":p:h:h")
-
---- @type ParserInfo[]
-local parser_configs
-
-if type(parsers.get_parser_configs) == "function" then
-  parser_configs = parsers.get_parser_configs()
-else
-  -- "nvim-treesitter.parsers" exports the parser table itself starting nvim-treesitter v1.x
-  parser_configs = parsers
-end
-
-parser_configs.ghostty = {
-  install_info = {
-    url = plugin_dir,
-    files = { "src/parser.c" },
-    branch = "main",
-  },
-  requires_generate_from_grammar = true,
-}
-
-
-
+-- Attempt to register the parser to nvim-treesitter, if installed.
+require("tree-sitter-ghostty").register_parser()
 
